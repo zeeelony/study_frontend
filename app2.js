a33cc9e8de521f6e8647f07abbe0215f6d6c9597
zeeelony <finik1500@gmail.com>
2024-08-06 14:47:44 +0600

    Delete app2.js

--- app2.js
+++
@@ -1,9 +0,0 @@
-function Reverse(inputString) {
-    let reversedString = '';
-    for (let i = inputString.length - 1; i >= 0; i--) {
-        reversedString += inputString[i];
-    }
-
-    return reversedString;
-}
-console.log(Reverse("123456789"))
