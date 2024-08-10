6d136f337f108f1b585f046aaedc38b0b6ab1450
zeeelony <finik1500@gmail.com>
2024-08-10 16:03:57 +0600

    Update modal.js

---
+++ modal.js
@@ -0,0 +1,49 @@
+// //MODAL
+
+const modal = document.querySelector(".modal");
+const modalTrigger = document.querySelector("#btn-get");
+const modalCloseButton = document.querySelector(".modal_close");
+
+const openModal = () => {
+    modal.style.display = "block";
+    document.body.style.overflow = "hidden";
+};
+
+const closeModal = () => {
+    modal.style.display = "none";
+    document.body.style.overflow = "";
+};
+
+const showModalByScroll = () => {
+    if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        openModal();
+        window.removeEventListener('scroll', showModalByScroll);
+    }
+};
+
+window.addEventListener('scroll', showModalByScroll);
+
+setTimeout(() => {
+    openModal();
+}, 10000);
+
+modalTrigger.onclick = () => openModal();
+modalCloseButton.onclick = () => closeModal();
+modal.onclick = (event) => {
+    if (event.target === modal) {
+        closeModal();
+    }
+};
+
+// Открытие модального окна при клике на кнопку
+modalTrigger.onclick = () => openModal();
+
+// Закрытие модального окна при клике на кнопку закрытия
+modalCloseButton.onclick = () => closeModal();
+
+// Закрытие модального окна при клике на его фон
+modal.onclick = (event) => {
+    if (event.target === modal) {
+        closeModal();
+    }
+};
