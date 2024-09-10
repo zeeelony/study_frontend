da48406f003fcc1c3ceb6ce82183a4fbb3504a1b
zeeelony <finik1500@gmail.com>
2024-09-10 15:17:01 +0600

    Add files via upload

---
+++ lesson2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MnainPage from './pages/MainPage';
+function App() {
+  return (
+ <>
+ <MainPage/>
+ <p>31231</p>
+ </>
+  );
+}
+
+export default App;
---
+++ lesson2/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <p>21</p>
+  </React.StrictMode>
+);
